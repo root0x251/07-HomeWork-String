4d1474f5d19cc19e46eb3a576f968c80ce67c678
Slava <rtootik@gmail.com>
2017-03-17 19:22:45 +0300

    Initial Commit

---
+++ 07-HomeWork-String/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  07-HomeWork-String
+//
+//  Created by Slava on 17.03.17.
+//  Copyright © 2017 Slava. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
